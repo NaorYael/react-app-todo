517edac959b479dd45bf4f61ba983440d04f9ae5
naor_y <naor_y@malam-payroll.com>
2022-07-11 16:01:42 +0300

    init app

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -2,12 +2,15 @@ import React from 'react';
 import useInputHook from './Hooks/UseInputHook'
 
 const TodoForm = ({addTodo}) => {
+  
   const [value, handleChange, resetValue] = useInputHook('');
+  
   const handleSubmit = (event) => {
     event.preventDefault();
     resetValue();
     addTodo(value);
   }
+  
   return (
     <div className="info">
       <form onSubmit={handleSubmit}>
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,21 +1,45 @@
 import React, { useState } from 'react';
+import useInputHook from './Hooks/UseInputHook';
 
-const TodoItem = ({todo, deleteTodo}) => {
- 
-  const [completed, setCompleted] = useState(todo.completed);
- 
+const TodoItem = ({todo, deleteTodo, toggleTodo, completed, editTodo}) => {
+  const [isEditMode, setIsEdit] = useState(false);
+
+  const [value, handleChange] = useInputHook(todo.text);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setIsEdit(false);
+    editTodo(todo.id, value);
+  }
+  
   return (
     <div className="list-item">
         <div className="box-input">
-          <input type="checkbox" defaultChecked={completed}/>
-          <div style={{textDecoration: completed ? 'line-through' : 'none'}}>
-            {todo.text}
-          </div>
+          <input 
+          onChange={() => toggleTodo(todo.id)} 
+          type="checkbox" 
+          defaultChecked={completed}
+          />
+          {isEditMode ? 
+          <form 
+          onSubmit={handleSubmit}
+          className="edit-form">
+            <input value={value} onChange={handleChange} type="text" autoFocus/>
+            <div>
+              <button type="submit" className="save">Save</button>
+            </div>
+          </form> 
+          : 
+          (  <div style={{textDecoration: completed ? 'line-through' : 'none'}}>
+          {todo.text}
+        </div>)
+          }
+        
         </div>
     
       <div>
-         <button className="edit">Edit</button>
-         <button onClick={() => deleteTodo(todo.id)} className="delete">Delete</button>
+         <button onClick={() => setIsEdit(!isEditMode)} className="edit">Edit</button>
+         <button onClick={() => window.confirm('Are you sure you wish to delete this todo?') ? deleteTodo(todo.id) : ''} className="delete">Delete</button>
       </div>
 
     </div>
